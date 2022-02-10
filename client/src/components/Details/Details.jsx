c1c9435045edd204720b2781f04ea580b3edf5f1
Eliana Alvarez <alvarez.eliana00@gmail.com>
2022-02-10 02:01:59 -0300

    realizando diseño responsive

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -5,7 +5,7 @@ import { getDetail } from "../../actions";
 import { useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
 import Button from "../Buttons/Button";
-import Loading from '../Loading/Loading'
+import Loading from "../Loading/Loading";
 import styles from "./Details.module.css";
 
 export default function Details(props) {
@@ -26,36 +26,32 @@ export default function Details(props) {
       </Link>
       <div className={styles.padreDiv}>
         {detailVideogame.name ? (
-          <div className={styles.divContenedor}>
-             <img
-              src={detailVideogame.background_image}
-              alt=""
-            />
-            <div className={styles.divSuper}>
+          <>
+            <div className={styles.divContenedor}>
               <div className={styles.divTitle}>
-               <h1>{detailVideogame.name}</h1>
+                <h1>{detailVideogame.name}</h1>
               </div>
-              <br/>
-              <br/>
-              <div className={styles.divUl}>
-                <div>
-                  <h3>Genres</h3>
-                  <ul key={Math.random(2, 9)}>
-                    {detailVideogame?.genres.map((g) => (
-                      <li key={Math.random(0, 10)}>{g.name}</li>
-                    ))}
-                  </ul>
+              <img src={detailVideogame.background_image} alt=""/>
+              <div className={styles.divSuper}>
+                <div className={styles.divUl}>
+                  <div>
+                    <h3>Genres</h3>
+                    <ul key={Math.random(2, 9)}>
+                      {detailVideogame?.genres.map((g) => (
+                        <li key={Math.random(0, 10)}>{g.name}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div>
+                    <h3>Platforms</h3>
+                    <ul key={Math.random(1, 5)}>
+                      {detailVideogame?.platforms.map((p) => (
+                        <li key={Math.random(0, 10)}>{p}</li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
-                <div>
-                  <h3>Platforms</h3>
-                  <ul key={Math.random(1,5)}>
-                    {detailVideogame?.platforms.map((p) => (
-                      <li key={Math.random(0, 10)}>{p}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className={styles.divRecargado}>
+                <div className={styles.divRecargado}>
                   <div>
                     <h3>Released</h3>
                     <h5>{detailVideogame.released}</h5>
@@ -64,17 +60,18 @@ export default function Details(props) {
                     <h3>Rating</h3>
                     <h5>{detailVideogame.rating}</h5>
                   </div>
+                </div>
+              </div>
+              <div className={styles.divAbout}>
+                <h1>About</h1>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: detailVideogame.description,
+                  }}
+                />
               </div>
             </div>
-            <div className={styles.divAbout}> 
-             <h1>About</h1> 
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: detailVideogame.description,
-                }}
-              />
-            </div>
-          </div>
+          </>
         ) : (
           <Loading />
         )}
