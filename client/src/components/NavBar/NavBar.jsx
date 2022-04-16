dcf311c3da1d83513bea90ba426bbfd3443f7942
Eliana Alvarez <alvarez.eliana00@gmail.com>
2022-04-16 00:28:10 -0300

    cambios en nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import "./NavBar.css";
-import ReorderIcon from '@material-ui/icons/Reorder';
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 export default function NavBar() {
-
   const [showLinks, setShowLinks] = useState(false);
 
   return (
     <div className="divNav">
-      <div className="leftSide" >
-        <div className= "divLinks" id={showLinks ? "hidden" : " "}>
-          <Link to="/">Welcome</Link>
-          <Link to="/home">Home</Link>
-          <Link to="/videogame">Create Videogame</Link>
+      <div className="leftSide">
+        <div className="divLinks" id={showLinks ? "hidden" : " "}>
+          <NavLink to="/">Welcome</NavLink>
+          <NavLink to="/home" activeClassName="is-active">
+            Home
+          </NavLink>
+          <NavLink to="/videogame" activeClassName="is-active">
+            Create Videogame
+          </NavLink>
         </div>
-        <button onClick={()=>setShowLinks(!showLinks)}><ReorderIcon/></button>
+        <button onClick={() => setShowLinks(!showLinks)}>
+          <ReorderIcon />
+        </button>
       </div>
       <div className="rightSide">
         <SearchBar />
