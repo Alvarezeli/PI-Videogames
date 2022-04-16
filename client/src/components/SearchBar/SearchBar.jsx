d2f84470167467ea9a972c9ef705bd0d68a9eacd
Eliana Alvarez <alvarez.eliana00@gmail.com>
2022-04-15 21:35:37 -0300

    modificaciones en el paginado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getNameVideogames } from "../../actions";
 import styles from './SearchBar.module.css';
 
